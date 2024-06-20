64b699bca65a99dc9b18fecd9d192c7b02c04a88
Felix Gu <duduisepic@gmail.com>
2024-06-19 21:22:36 -0700

    main args

--- fibonacci.py
+++ fibonacci.py
@@ -1,29 +1,24 @@
 from argparse import ArgumentParser, Namespace
 print('Hello World, I\'m Felix')
-
-def main():
-    print('Main method')
     
 if __name__ =='__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--nth_term', help='The nth term of a fibonacci sequence', type=int)
+    args: Namespace = parser.parse_args()
 
-parser = ArgumentParser()
-parser.add_argument('nth_term', help='The nth term of a fibonacci sequence', type=int)
-args: Namespace = parser.parse_args()
-
-fib = 1
-fib1 = 1
-fib2 = 1
-if args.nth_term == 1 or args.nth_term == 2:
-    print('1')
-else:
-    print('1')
-    for n in range(2,args.nth_term):
-        print(f'{fib}')
-        fib = fib1 + fib2
-        fib1 = fib2
-        fib2 = fib
-    print(f'{fib}')
-    fib1 = 0
+    fib = 1
+    fib1 = 1
     fib2 = 1
-    fib = 0
+    if args.nth_term == 1 or args.nth_term == 2:
+        print('1')
+    elif args.nth_term != None:
+        print('0')
+        for n in range(2,args.nth_term):
+            print(f'{fib}')
+            fib = fib1 + fib2
+            fib1 = fib2
+            fib2 = fib
+        print(f'{fib}')
+        fib1 = 0
+        fib2 = 1
+        fib = 0
